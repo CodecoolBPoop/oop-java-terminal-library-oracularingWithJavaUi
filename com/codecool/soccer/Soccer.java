cf2e51bf19d193eca576389b8b7d0b27418bf6f5
SzilardZ <szilard.zr@gmail.com>
2018-06-27 13:00:49 +0200

    working program exit function

--- com/codecool/soccer/Soccer.java
+++ com/codecool/soccer/Soccer.java
@@ -8,13 +8,13 @@ public class Soccer {
 
 	static String[] menuHeader = {"Choose from these choices", "Choose a team", "Statistics", "Winner prediction"};
 	
-	static String[] mainMenu = {"1. Statistics", "2. Who would win?"};
+	static String[] mainMenu = {"1. Statistics", "2. Who would win?", " ", "0. Go back to Main menu"};
 
-	static String[] testTeams = {"1. Spain", "2. Italy", "3. Portugal", "4. Brasil", "5. Croatia", "7. Uruguay", "8. Iceland"};
+	static String[] testTeams = {"1. Spain", "2. Italy", "3. Portugal", "4. Brasil", "5. Croatia", "7. Uruguay", "8. Iceland", " ", "0. Go back to Main menu"};
 
-	static String[] testSpainStatistics = {"Goals: 5", "Received goals: 4", "Ball posession: 57%", "Fifa Ranking: 6"};
+	static String[] testSpainStatistics = {"Goals: 5", "Received goals: 4", "Ball posession: 57%", "Fifa Ranking: 6", " ", "0. Go back to Main menu"};
 
-	static String[] testItalyStatistics = {"Goals: 3", "Received goals: 3", "Ball posession: 52%", "Fifa Ranking: 11"};
+	static String[] testItalyStatistics = {"Goals: 3", "Received goals: 3", "Ball posession: 52%", "Fifa Ranking: 11", " ", "0. Go back to Main menu"};
 
 
 
@@ -32,7 +32,7 @@ public class Soccer {
 		}
 
 		System.out.println();
-		System.out.println("0. Exit");
+		System.out.println("E. Exit program");
     
 	}
 
@@ -47,8 +47,10 @@ public class Soccer {
 
 
 
-	static void handleUserInput() {
+	static String handleUserInput() {
 		String answer = getUserChoice();
+		String status = "keepGoing";
+		
 		if (answer.equals("1")) {
 			displayMenu(testTeams, menuHeader[1]);
 
@@ -63,18 +65,38 @@ public class Soccer {
 		} else if (answer.equals("3")) {
 			System.out.println("it is 3");
 		} else if (answer.equals("0")) {
-			displayMenu(mainMenu, menuHeader[0]);
+			menuLoop();
+		} else if (answer.equals("e")) {
+			System.out.println("vdvbdfg");
+			status = "exit";
+			System.exit(0);			
 		} else {
 			System.out.println("invalid");
+			handleUserInput();
 		}
+		
+		return status;
+	}
+	
+	
+	public static void menuLoop() {
+		int program = 1;
+		displayMenu(mainMenu, menuHeader[0]);
+		while(program > 0) {			
+			String active = handleUserInput();
+			if (active.equals("exit")) {
+				System.out.println("df");
+				program --;
+			}
+		}
+
 	}
 	
 
-    public static void main(String[] args){
-//        com.codecool.termlib.Terminal term = new com.codecool.termlib.Terminal();
-		displayMenu(mainMenu, menuHeader[0]);	
-		handleUserInput();
-        
+    public static void main(String[] args) {
+//      com.codecool.termlib.Terminal term = new com.codecool.termlib.Terminal();
+		menuLoop();
+		
     }
 
 }
